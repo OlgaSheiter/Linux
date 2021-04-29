6ecbc9b34dde19d96bea5185ee4349136d277776
Olga Sheiter <Sonua@yandex.ru>
2021-04-29 19:52:11 +0300

    Add Script.sh

---
+++ Script.sh
@@ -0,0 +1,9 @@
+!/bin/bash
+mkdir Test
+cd Test
+mkdir folder1 folder2 folder3
+cd folder1
+touch file1.txt file2.txt file3.txt file4.json file5.json
+mkdir Test1 Test2 Test3
+ls
+mv file1.txt file2.txt Test1
